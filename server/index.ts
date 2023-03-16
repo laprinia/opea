d3c4c9c234368756989acf95446d4c9ed54f6b32
laprinia <lavinialoghin7@gmail.com>
2023-03-16 10:46:36 +0200

    Added api rules

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,28 +8,50 @@ dotenv.config();
 
 const app: Express = express();
 
+const port = process.env.PORT;
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
 mongoose
   .connect(config.mongo.url, { retryWrites: true, w: "majority" })
   .then(() => {
     console.log("[opea-server]: Connected to MongoDB");
+    startServer();
   })
   .catch((e) => {
     console.log(e);
   });
 
-const port = process.env.PORT;
-const corsOptions = {
-  origin: "http://localhost:3000",
+const startServer = () => {
+  app.use((req, res, next) => {
+    console.log(
+      `[opea-server]: Method: [${req.method}] - Url: [${req.url}] - IP[${req.socket.remoteAddress}]`
+    );
+    res.on("finish", () => {
+      console.log(
+        `[opea-server]: Method: [${req.method}] - Url: [${req.url}] - IP[${req.socket.remoteAddress}]`
+      );
+    });
+    next();
+  });
+
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hi!");
+  });
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error("Resource not found");
+    console.error(error);
+
+    return res.status(404).json({ message: error.message });
+  });
+
+  app.listen(port, () => {
+    console.log(`[opea-server]: Server is running at http://localhost:${port}`);
+  });
 };
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("hi!");
-});
-
-app.listen(port, () => {
-  console.log(`[opea-server]: Server is running at http://localhost:${port}`);
-});
